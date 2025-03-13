d37f95cdd20fb37a4e83a04bb86d1221b65ac2ee
Daisuke-lab <kikuchidaisuke.zr@gmail.com>
2025-03-12 21:02:40 -0500

    LAB8

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,13 +4,23 @@ import { createContext, useContext, useState } from "react";
 import PostType from "../types/PostType";
 import { Button } from "@mui/material";
 
+interface PostContextType {
+    selectedPostId: number,
+    setSelectedPostId: React.Dispatch<React.SetStateAction<number>>
+}
 
-export const PostConetxt = createContext(0)
+export const PostConetxt = createContext<PostContextType | null>(null)
 
 export default function Dashboard() {
+    const [selectedPostId, setSelectedPostId] = useState(0)
+    const postContextValue = {
+        selectedPostId,
+        setSelectedPostId
+    }
+    
     return (
         <div>
-            <PostConetxt.Provider value={0}>
+            <PostConetxt.Provider value={postContextValue}>
             <h1>Hello World</h1>
             <Posts/>
             </PostConetxt.Provider>
